0f25b92d72834fc80eddc97c267c99effe6d4eed
Nightmare <90391492+SINON3@users.noreply.github.com>
2023-09-13 14:46:50 +0530

    Some sht txt

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You Must Join in My Channel to use me\n\njoin Channel With The Link in Below</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You Must Join in My Channel To Use Me\n\nJoin Channel With The Link in Below</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
